ac63f32bb2c68f7ea3a662d2683634ed65c0b255
iamjd98 <iamjd98@gmail.com>
2023-01-20 15:57:14 +0800

    modified:   index.html 	modified:   myscript.js 	modified:   styles.css

--- myscript.js
+++ myscript.js
@@ -17,17 +17,22 @@ audio.src = "./assets/beep.mp3";
                 // </ul>
            // </div> 
 
-
+const orderidarray = [];
 const orderitemsarray = [];
 const orderpricearray = [];
 const orderarray = [];
 
-function orderbasket(itemname, itemprice) {
+let i = 0;
 
+function orderbasket(itemname, itemprice) {
+    
+
+orderidarray.push(i);    
 orderitemsarray.push (itemname);
 orderpricearray.push (itemprice);
 orderarray.push (itemname, itemprice);
 
+i++;
 
 let orderlist = document.getElementById('orderlist');
 
@@ -43,11 +48,33 @@ orderitempricespan.className = 'text-danger'
 
 orderitempricespan.appendChild(orderitemprice);
 
-orderitem.appendChild(orderitemname);
+const deletebutton = document.createElement('button');
+const deletebuttontext = document.createTextNode('Remove');
 
-orderitem.appendChild(orderitempricespan);
+deletebutton.setAttribute('onclick', 'deleteitem('+i+', this)');
+
+deletebutton.appendChild(deletebuttontext);
+deletebutton.className = 'btn btn-danger btn-sm m-2';
+
+const orderitemleftsidespan = document.createElement('span');
+
+orderitemleftsidespan.appendChild(orderitemname);
+
+orderitemleftsidespan.appendChild(orderitempricespan);
+
+
+orderitem.appendChild(orderitemleftsidespan);
+
+
+orderitem.appendChild(deletebutton);
+
 
 orderlist.appendChild(orderitem);
+
+console.log(orderidarray);
+
+totalitems(); 
+costitems();
 
 }
 
@@ -61,18 +88,45 @@ function costitems() {
     }
     else {
     document.getElementById("totalcost").innerText = orderpricearray.reduce(sumarray).toFixed(2);
+    
+    document.getElementById('amount').value = orderpricearray.reduce(sumarray).toFixed(2);
+
+    
     function sumarray(total, sum){
         return total + sum;
-    };
+
+        };
 };
 };
 
+
+
 function orderbasketclear() {
     let orderlist = document.getElementById('orderlist');
+    document.getElementById('amount').value = 0;
     orderlist.innerHTML = "";
     orderitemsarray.length = 0;
     orderpricearray.length = 0;
     orderarray.length = 0;
+    orderidarray.length = 0;
+    i = 0;
     totalitems();
     costitems ();
+}
+
+function deleteitem(orderid, button){
+    const indexnum = orderidarray.indexOf(orderid);
+    orderidarray.splice(indexnum, 1);
+    orderitemsarray.splice(indexnum, 1);
+    orderpricearray.splice(indexnum, 1);
+    console.log(orderidarray);   
+    totalitems();
+    costitems ();
+    console.log(button);
+    console.log(button.parentElement);
+    orderlist.removeChild(button.parentElement);
+
+    if (orderpricearray,length === 0) {
+        document.getElementById('amount').value = 0;
+    }
 }
